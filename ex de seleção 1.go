1f8e7cb7d352e6e67f0656832b9e88efeb62a6e4
Eduardo Gondim Marinho <eduardo.gondimm@dinfor.uniceub>
2023-03-20 10:35:03 -0300

    "ex de sele\303\247\303\243o 1.go"

---
+++ ex de seleção 1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b int
+
+	fmt.Print(10)
+	fmt.Scan(&a)
+	fmt.Print(5)
+	fmt.Scan(&b)
+
+	if (a>b) {
+		a=a-b
+		fmt.Println("a difernça dos numeros digitados")
+	} else if a=b-a;
+		fmt.Println("a diferença dos numeros digitados")
+}return (0)
+
